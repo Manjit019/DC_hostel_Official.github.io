2ec3f5174df545b2560f977f4356f6c76605bfb7
Manjit <manjitkumar1725@gmail.com>
2024-05-21 15:13:14 +0530

    Huge changes in all

--- script.js
+++ script.js
@@ -1,53 +1,159 @@
 
+//scroll to top
+const scrolltopBtn = document.getElementById("scroll-top");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    scrolltopBtn.style.bottom = "2rem";
+    // scrolltopBtn.style.right = "4rem";
+  }
+  else {
+    scrolltopBtn.style.bottom = "-4rem";
+  }
+}
+scrolltopBtn.addEventListener('click', () => {
+  window.scrollTo(0, 0);
+})
+
 //loader component
-const loader=document.querySelector(".loader-wrapper");
-window.addEventListener("load",()=>{
-    setTimeout(function(){
-        loader.style.display="none";
-    },2000);
+const loader = document.querySelector(".loader-wrapper");
+window.addEventListener("load", () => {
+  setTimeout(function () {
+    loader.style.display = "none";
+  }, 2000);
 })
 
 // Creating a responsive navbar component
 
-const hamburger=document.querySelector(".three-dot");
-const headerElem=document.querySelector(".header");
+const hamburger = document.querySelector(".three-dot");
+const headerElem = document.querySelector(".header");
 
-hamburger.addEventListener('click', ()=>{
-    headerElem.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+  headerElem.classList.toggle("active");
 })
 
-//scroll to top
-const scrolltopBtn=document.getElementById("scroll-top");
+//carousol slideshow
+let slideIndex = 0;
+let timeout;
+const slides = document.querySelectorAll('.slide');
+const indicators = document.querySelectorAll('.indicator');
 
-window.onscroll=function(){
-   if(document.body.scrollTop>50 || document.documentElement.scrollTop>50){
-    scrolltopBtn.style.bottom="1rem";
-   }
-   else{
-    scrolltopBtn.style.bottom="-2rem";
-   }
+function showSlides() {
+  slides.forEach((slide) => {
+    slide.style.display = 'none';
+  });
+  indicators.forEach((indicator) => {
+    indicator.classList.remove('active');
+  });
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  indicators[slideIndex - 1].classList.add('active');
+  timeout = setTimeout(showSlides, 2000);
 }
-scrolltopBtn.addEventListener('click',()=>{
-    window.scrollTo(0,0);
-})
+
+function plusSlides(n) {
+  clearTimeout(timeout);
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  clearTimeout(timeout);
+  showSlides(slideIndex = n);
+}
+
+showSlides(); // Start the slideshow
+
+//Notice element
+const container = document.querySelector('.notice-field');
+const notices = document.querySelectorAll('.all-notices');
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
+let positionX = 0;
+let positionY = 0;
+let animationId;
+function moveNotices() {
+  if (window.innerWidth <= 850) {
+    positionY--;
+    container.style.transform = `translateY(${positionY}px)`;
+    if (positionY <= -containerHeight) {
+      positionY = containerHeight; // Start from the end
+    }
+  }
+  else {
+    positionX--;
+    container.style.transform = `translateX(${positionX}px)`;
+    if (positionX <= -containerWidth) {
+      positionX = containerWidth; // Start from the end
+    }
+  }
+
+  animationId = requestAnimationFrame(moveNotices);
+}
+
+// if (window.innerWidth > 568) {
+//   moveNotices();
+// }
+
+function pauseAnimation() {
+  cancelAnimationFrame(animationId);
+}
+
+function resumeAnimation() {
+  moveNotices();
+}
+moveNotices();
+container.addEventListener('mouseenter', pauseAnimation);
+container.addEventListener('mouseleave', resumeAnimation);
+
+
+// view more component 
+const ViewMoreBtn = document.querySelector('.view-more--btn');
+let current = document.querySelector(".Section-gallery");
+
+ViewMoreBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+  var extraImages = document.querySelectorAll('.extra');
+  for (var i = 0; i < extraImages.length; i++) {
+    if (extraImages[i].style.display === 'none') {
+      extraImages[i].style.display = 'block';
+      ViewMoreBtn.querySelector("a").textContent = "View Less ▲";
+    } else {
+      extraImages[i].style.display = 'none';
+      ViewMoreBtn.querySelector("a").textContent = "View More ▼";
+      current.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+  ViewMoreBtn.scrollIntoView({ behavior: 'smooth', block: 'end'});
+  // // Scroll to the last extra image
+  // const lastExtraImage = extraImages[extraImages.length - 1];
+  // lastExtraImage.scrollIntoView({ behavior: 'smooth', block: 'end' });
+
+  // if (ViewMoreBtn.querySelector("a").textContent === "View Less ▲") {
+    
+  // }
+
+});
 
 
 //gallery click to download image 
-const downloadbtn=document.querySelectorAll('[data-download]');
+const downloadbtn = document.querySelectorAll('[data-download]');
 
-downloadbtn.forEach(i =>{
-    const id=i.dataset.download;
-    const image=document.getElementById(id);
-    const a=document.createElement("a");
+downloadbtn.forEach(i => {
+  const id = i.dataset.download;
+  const image = document.getElementById(id);
+  const a = document.createElement("a");
 
-    a.href=image.src;
-    a.download="";
-    a.style.display="none";
+  a.href = image.src;
+  a.download = "";
+  a.style.display = "none";
 
-    i.addEventListener('click',()=>{
-        // console.log(image.childNodes[1]);
-        a.click();
-        a.removeChild(a);  
-    });
+  i.addEventListener('click', () => {
+    // console.log(image.childNodes[1]);
+    a.click();
+    a.removeChild(a);
+  });
 });
 
